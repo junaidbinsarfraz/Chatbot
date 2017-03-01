63d22edd5d1e8fa71ed9fff8a4ad5f39b297c4e6
AM GIMINT (Farhan Hamayun) <farhanhamayun@INT.GOV.PK>
2017-03-01 15:16:22 +0500

    Login in progress

---
+++ Chatbot/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chatbot.Models;
+
+namespace Chatbot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        // Show login page if user is not loggedin
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Do login for a user and redirect to specific page w.r.t. user role
+        [HttpPost]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // LoyaltyContainer dataContext = new LoyaltyContainer();
+                // Check credentials
+                //User user = dataContext.Users.FirstOrDefault(u => u.Username == loginModel.Username && u.Password == loginModel.Password && u.Status == true);
+
+                User user = new User();
+
+                if (user != null)
+                {
+                    HttpContext.Session["LoggedInUser"] = user;
+
+                    return RedirectToAction("Index", "Home");
+                }
+                // Invalid credentials
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password");
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Chatbot/Models/SignupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatbot.Models
+{
+    public class SignupModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public Int64 Age { get; set; }
+    }
+}
